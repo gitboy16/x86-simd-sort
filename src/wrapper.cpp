de2e3c8a6d6e53040a9923b8c28df9d0efaddb5a
gitboy16 <82724369+gitboy16@users.noreply.github.com>
2025-08-27 12:21:35 +0100

    Update wrapper.cpp

--- src/wrapper.cpp
+++ src/wrapper.cpp
@@ -1,11 +1,12 @@
-#include "src/x86simdsort-static-incl.h"
+#include "x86simdsort-static-incl.h"
+#include <stdbool.h>
 
 // g++ wrapper.cpp -mavx512f -mavx512dq -mavx512vl -O3 -o libx86simdsort.dll -shared -I./x86-simd-sort -fopenmp -DXSS_USE_OPENMP
 
-extern "C" void qsort_float(float *arr, size_t size) {
-    x86simdsortStatic::qsort(arr, size, false, true);
+extern "C" void qsort_float(float *arr, size_t size, bool incr) {
+    x86simdsortStatic::qsort(arr, size, false, incr);
 }
 
-extern "C" void qsort_double(double *arr, size_t size) {
-    x86simdsortStatic::qsort(arr, size, false, true);
+extern "C" void qsort_double(double *arr, size_t size, bool incr) {
+    x86simdsortStatic::qsort(arr, size, false, incr);
 }
